bf3d1a7f1923d0ff4183f6e45e023d46581ce411
L. Guo <iml885203@gmail.com>
2016-04-13 13:56:08 +0800

    ajax event 改寫

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+    var $window = $(window);
     //URL function
     $.UrlParam = function(name) {
             //宣告正規表達式
@@ -18,43 +19,121 @@ $(function() {
     var url = window.location.protocol + '//' + window.location.hostname + window.location.pathname;
 
     function switchPage(i) {
-      var fadein = function(){
-        if($(window).width() < '768'){
-          $('.surface img').attr('src','images/surface_backup.png');
-          $('.blurry').attr('src','images/surface_backup_blurry.png');
+        function ajaxUnbild(){
+          $window.unbind('scroll');
+
+          /*insert up effect*/
+          $window.on('scroll resize', check_if_in_view);
+          /*show scroll up*/
+          $window.on('scroll', function() {
+              var scrollTop = $(this).scrollTop();
+              /*scroll top*/
+              if (scrollTop >= 200) {
+                  $('.scrollup').fadeIn().css("display", "flex");;
+              } else {
+                  $('.scrollup').fadeOut();
+              }
+              var window_bottom_position = ($(this).scrollTop() + $(this).height());
+          });
         }
-        $(this).fadeIn(500,function(){
-          /*這邊寫換頁動畫*/
-          var oao = $('.oao');
-          oao.animate({fontSize : '3em'}, 1000);
-          oao.animate({fontSize : '1em'}, 1000);
-        });
-      };
         switch (i) {
             case '1':
-                $('.content').fadeOut(500,function(){
-                  $(this).load('layout/home.html',fadein);
+                $('.content').fadeOut(500, function() {
+                    $(this).load('layout/home.html', function() {
+                        ajaxUnbild();
+                        if ($window.width() < '768') {
+                            $('.surface img').attr('src', 'images/surface_backup.png');
+                            $('.blurry').attr('src', 'images/surface_backup_blurry.png');
+                        }
+
+                        /*home*/
+                        $window.bind('scroll', function() {
+                            var scrollTop = $(this).scrollTop();
+                            /*home image blurry*/
+                            var $blurry = $('.blurry');
+                            var imageBottom = ($blurry.outerHeight() + $blurry.offset().top);
+                            var blurryPosition = scrollTop / (imageBottom / 3 * 2);
+                            $blurry.css('opacity', (blurryPosition <= 1) ? blurryPosition : 1);
+                        });
+                        $(this).fadeIn(500, function() {
+                            /*這邊寫換頁動畫*/
+                            var oao = $('.oao');
+                            oao.animate({
+                                fontSize: '3em'
+                            }, 1000);
+                            oao.animate({
+                                fontSize: '1em'
+                            }, 1000);
+                        });
+                    });
                 });
+
                 break;
             case '4':
-                $('.content').fadeOut(500,function(){
-                  $(this).load('layout/instruction.html',fadein);
+                $('.content').fadeOut(500, function() {
+                    $(this).load('layout/instruction.html', function() {
+                      ajaxUnbild();
+                      /*我不知道這個要擺哪邊得斯*/
+                      $('.arrow-next').bind('click', function() {
+                          var $currentSlide = $('.active-slide');
+                          var nextSlide = $currentSlide.next();
+                          console.log(nextSlide);
+                          var $currentDot = $('.active-dot');
+                          var nextDot = $currentDot.next();
+
+                          $currentSlide.fadeOut(500).removeClass('active-slide');
+                          nextSlide.fadeIn(500).addClass('active-slide');
+                          $currentDot.removeClass('active-dot');
+                          nextDot.addClass('active-dot');
+                          return 0;
+                      });
+                      $('.arrow-prev').bind('click', function() {
+                          var $currentSlide = $('.active-slide');
+                          var prevSlide = $currentSlide.prev();
+                          var $currentDot = $('.active-dot');
+                          var prevDot = $currentDot.prev();
+
+                          $currentSlide.fadeOut(500).removeClass('active-slide');
+                          prevSlide.fadeIn(500).addClass('active-slide');
+                          $currentDot.removeClass('active-dot');
+                          prevDot.addClass('active-dot');
+                      });
+                        $(this).fadeIn(500, function() {
+                            /*這邊寫換頁動畫*/
+                        });
+                    });
                 });
                 break;
             case '5':
-                $('.content').fadeOut(500,function(){
-                  $(this).load('layout/aspect.html',fadein);
+                $('.content').fadeOut(500, function() {
+                    $(this).load('layout/aspect.html', function() {
+                      ajaxUnbild();
+                      /*aspect*/
+
+                      $(this).fadeIn(500, function() {
+                          /*這邊寫換頁動畫*/
+                      });
+                    });
                 });
                 break;
             case '6':
-                $('.content').fadeOut(500,function(){
-                  $(this).load('layout/aboutus.html',fadein);
+                $('.content').fadeOut(500, function() {
+                    $(this).load('layout/aboutus.html', function() {
+                      ajaxUnbild();
+                      /*aspect*/
+
+                      $(this).fadeIn(500, function() {
+                          /*這邊寫換頁動畫*/
+                      });
+                    });
                 });
                 break;
         }
         $('.menu ul li, .mobile-menu ul li').removeClass('active');
         $('.menu ul li:nth-child(' + i + '), .mobile-menu ul li:nth-child(' + i + ')').addClass('active');
+        index = $.UrlParam('index');
     };
+    /*switch page*/
     var index = $.UrlParam('index');
     if (!!index) {
         switchPage(index);
@@ -75,83 +154,47 @@ $(function() {
     };
     $('.menu ul li, .mobile-menu ul li').on('click', clickFunction);
 
-    $('.mobile-menu-button').on('click', function(){
-      $('body').animate({right: '200px'}, 'slow');
-      $('.veil').fadeIn('slow');
+    $('.mobile-menu-button').on('click', function() {
+        $('body').animate({
+            right: '200px'
+        }, 'slow');
+        $('.veil').fadeIn('slow');
     });
-    $('.veil').on('click', function(){
-      $('body').animate({right: '0px'}, 'slow');
-      $('.veil').fadeOut('slow');
+    $('.veil').on('click', function() {
+        $('body').animate({
+            right: '0px'
+        }, 'slow');
+        $('.veil').fadeOut('slow');
     });
-    $('.scrollup').on('click', function(){
-      $('html, body').animate({scrollTop: 0}, 'slow');
-    });
-
-    /*home*/
-    $(window).on('scroll', function() {
-      var scrollTop = $(this).scrollTop();
-      /*image blurry*/
-      var $blurry = $('.blurry');
-      var imageBottom = ($blurry.outerHeight() + $blurry.offset().top);
-      var blurryPosition = scrollTop/(imageBottom/3*2);
-      $blurry.css('opacity', (blurryPosition<=1)?blurryPosition:1);
-      /*scroll top*/
-      if(scrollTop >= 200){
-        $('.scrollup').fadeIn().css("display","flex");;
-      }
-      else{
-        $('.scrollup').fadeOut();
-      }
-      var window_bottom_position = ($(this).scrollTop() + $(this).height());
-
-    });
-    /*我不知道這個要擺哪邊得斯*/
-    $('.arrow-next').on('click',function() {
-      var $currentSlide = $('.active-slide');
-      var nextSlide = $currentSlide.next();
-      var $currentDot = $('.active-dot');
-      var nextDot = $currentDot.next();
-
-      $currentSlide.fadeOut(500).removeClass('active-slide');
-      nextSlide.fadeIn(500).addClass('active-slide');
-      $currentDot.removeClass('active-dot');
-      nextDot.addClass('active-dot');
-    });
-    $('.arrow-prev').on('click',function() {
-      var $currentSlide = $('.active-slide');
-      var prevSlide = $currentSlide.prev();
-      var $currentDot = $('.active-dot');
-      var prevDot = $currentDot.prev();
-
-      $currentSlide.fadeOut(500).removeClass('active-slide');
-      prevSlide.fadeIn(500).addClass('active-slide');
-      $currentDot.removeClass('active-dot');
-      prevDot.addClass('active-dot');
+    $('.scrollup').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'slow');
     });
 
-    /*animation-element*/
-    var $window = $(window);
 
+
+
+    /*insert up*/
     function check_if_in_view() {
-      var window_height = $window.height();
-      var window_top_position = $window.scrollTop();
-      var window_bottom_position = (window_top_position + window_height);
-      var $animation_elements = $('.insertup');
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+        var $animation_elements = $('.insertup');
+        $.each($animation_elements, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
 
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-          (element_top_position <= window_bottom_position)) {
-          $element.addClass('insert');
-        } else {
-          $element.removeClass('insert');
-        }
-      });
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('insert');
+            } else {
+                $element.removeClass('insert');
+            }
+        });
     };
-    $window.on('scroll resize', check_if_in_view);
 
 });
