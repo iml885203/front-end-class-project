efe4432f149ece65d6c8b641e759556e791a9393
L. Guo <iml885203@gmail.com>
2016-04-19 14:25:33 +0800

    header透明化

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=320px; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
     <title>北科小角落</title>
      <link rel="icon" href="images/icon.png" type="image/png" sizes="32x32">
-    <link rel="stylesheet/less" type="text/css" href="css/index.less">
-    <!-- <link rel="stylesheet" type="text/css" href="css/index.css"> -->
+    <!-- <link rel="stylesheet/less" type="text/css" href="css/index.less"> -->
+    <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 
 <body>
@@ -32,7 +32,7 @@
                 <li>角落黑白面</li>
                 <li>關於我們</li>
             </ul>
-            <a href="#" class="mobile-menu-button effect">
+            <a href="javascript: void(0)" class="mobile-menu-button effect">
                 <span class="bar"></span>
             </a>
         </div>
@@ -99,7 +99,7 @@
 </body>
 <script src="js/jquery-1.12.0.min.js"></script>
 <script src="js/masonry.pkgd.min.js"></script>
-<script src="js/less.min.js"></script>
+<!-- <script src="js/less.min.js"></script> -->
 <script src="js/index.js"></script>
 
 
