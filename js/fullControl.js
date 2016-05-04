fc89a92272c01f1d513f0d2dead60a4333a091b4
L. Guo <iml885203@gmail.com>
2016-05-04 14:58:34 +0800

    改用active判斷

--- js/fullControl.js
+++ js/fullControl.js
@@ -2,9 +2,15 @@ $(function(){
   var $window = $(window);
   var $main = $('.main');
   var windowHeight = 0;
+  var scrollHeight = 0;
   var scrollIndex = 0;
   var scrollFinsh = 5;
-  $main.children('selector')
+  $main.children('view').each(function(){
+    console.log("123");
+    if($(this).hasClass('active')){
+      scrollHeight = ($(this).index() + 1) * windowHeight;
+    }
+  });
 
   $window.on('load resize', function(){
     $('body').height(windowHeight = $(this).height());
@@ -14,18 +20,30 @@ $(function(){
     $main.animate({scrollTop: 0});
   })
   $window.on('keyup', function(e){
-    if(e.keyCode == 83){ //press S
-      console.log("press S");
-      if(!$main.is(':animated')){
-        $main.animate({scrollTop: $main.scrollTop()+windowHeight});
+    if(!$main.is(':animated')){
+      if(e.keyCode == 83){ //press S
+        console.log("press S");
+        $main.children('.view').each(function(){
+          if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).next('.view').addClass('active');
+            scrollHeight = ($(this).next('.view').index()) * windowHeight;
+            return false;
+          }
+        });
       }
-
-    }
-    else if(e.keyCode == 87){ //press W
-      console.log("press W");
-      if(!$main.is(':animated')){
-        $main.animate({scrollTop: $main.scrollTop()-windowHeight});
+      else if(e.keyCode == 87){ //press W
+        console.log("press W");
+        $main.children('.view').each(function(){
+          if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).prev('.view').addClass('active');
+            scrollHeight = ($(this).prev('.view').index()) * windowHeight;
+            return false;
+          }
+        });
       }
+      $main.animate({scrollTop: scrollHeight});
     }
   });
   $window.on('DOMMouseScroll mousewheel', function(e){
